23dca79329058fc36c35db73c50beee6cab15c8a
junchh <cjunho9868@gmail.com>
2025-12-23 22:49:39 +0700

    feat: almost done with file reading but there's a huge bug with the print function

--- src/main.zig
+++ src/main.zig
@@ -31,7 +31,7 @@ pub fn main() !void {
         try stdout.writeAll("1. Create a database \n");
         try stdout.writeAll("2. Load a database \n");
         try stdout.writeAll("-1. End session \n");
-        try stdout.writeAll("Pick your action: ");
+        try stdout.writeAll("Pick your action database action: ");
         try stdout.flush();
 
         const str_optional = try stdin.takeDelimiter('\n');
@@ -85,23 +85,71 @@ pub fn main() !void {
         }
 
         const file_reader = file_reader_temp orelse break;
-        _ = file_reader;
         const file_writer = file_writer_temp orelse break;
-        _ = file_writer;
         const selected_db_string = db_optional orelse break;
 
-        var cur_index: u32 = 0;
-        var cur_max: u32 = 16;
+        const book_file_read = try file_reader.file.readToEndAlloc(allocator, 10 * 1024 * 1024);
+
+        const len_existing = book_file_read.len / 384;
+
+        var cur_index: u64 = 0;
+        var cur_max: u64 = 16;
         var books = try allocator.alloc(Book, cur_max);
+
+        if (len_existing != 0) {
+            cur_index = len_existing;
+            cur_max = len_existing * 2;
+            books = try allocator.realloc(books, cur_max);
+
+            {
+                var i: u64 = 0;
+                while (i < book_file_read.len) : (i += 384) {
+                    var len_title: u64 = 0;
+                    var len_description: u64 = 0;
+
+                    var k: u64 = 0;
+                    while (k < 384) : (k += 1) {
+                        if (k < 128) {
+                            if (book_file_read[k] != 0) {
+                                len_title += 1;
+                            }
+                        } else {
+                            if (book_file_read[k] != 0) {
+                                len_description += 1;
+                            }
+                        }
+                    }
+                    const title = try allocator.alloc(u8, len_title);
+                    const description = try allocator.alloc(u8, len_description);
+                    k = 0;
+                    while (k < 384) : (k += 1) {
+                        if (k < 128) {
+                            if (book_file_read[k] != 0) {
+                                title[k] = book_file_read[k];
+                            }
+                        } else {
+                            if (book_file_read[k] != 0) {
+                                description[k - 128] = book_file_read[k];
+                            }
+                        }
+                    }
+
+                    books[i / 384] = .{ .title = title, .description = description };
+                }
+            }
+        }
 
         while (true) {
             try stdout.print("Current Selected DB: {s}\n", .{selected_db_string});
+
+            try stdout.flush();
             try stdout.writeAll("List of Actions: \n");
             try stdout.writeAll("1. Add a book \n");
             try stdout.writeAll("2. Update a book \n");
             try stdout.writeAll("3. Delete a book \n");
             try stdout.writeAll("4. Save \n");
-            try stdout.writeAll("-1. End database session \n");
+            try stdout.writeAll("5. Print books \n");
+            try stdout.writeAll("-1. Disconnect with database \n");
 
             try stdout.writeAll("\n");
             try stdout.writeAll("Pick your action: ");
@@ -216,7 +264,45 @@ pub fn main() !void {
                     try stdout.writeAll("Book deleted!.\n");
                     try stdout.flush();
                 },
-                '4' => {},
+                '4' => {
+                    try file_writer.file.seekTo(0);
+                    var book_file = try allocator.alloc(u8, cur_index * 384);
+                    {
+                        var i: u16 = 0;
+                        while (i < cur_index) : (i += 1) {
+                            var j: u64 = i * 384;
+                            while (j < (i + 1) * 384) : (j += 1) {
+                                // i = 0
+                                // j = 0 -> 383
+                                // i = 1
+                                // j = 384 -> 767
+                                const norm_j = j % 384;
+                                if (norm_j < books[i].title.len) {
+                                    book_file[j] = books[i].title[norm_j];
+                                } else if (norm_j >= 128 and norm_j < 128 + books[i].description.len) {
+                                    book_file[j] = books[i].description[norm_j - 128];
+                                } else {
+                                    book_file[j] = 0;
+                                }
+                            }
+                        }
+                        try file_writer.file.writeAll(book_file);
+                    }
+                    allocator.free(book_file);
+                },
+                '5' => {
+                    try stdout.print("Number of books: {d}\n", .{cur_index});
+                    try stdout.flush();
+
+                    {
+                        var i: u16 = 0;
+                        while (i < cur_index) {
+                            try stdout.print("Book number: {d}\ntitle: {s}\ndescription: {s}\n", .{ (i + 1), books[i].title, books[i].description });
+                            try stdout.flush();
+                            i += 1;
+                        }
+                    }
+                },
                 else => {
                     try stdout.writeAll("That action doesn't exist!\n");
                     try stdout.flush();
@@ -224,21 +310,16 @@ pub fn main() !void {
             }
         }
 
-        var i: u16 = 0;
-        while (i < cur_index) {
-            try stdout.print("Book number: {d}\ntitle: {s}\ndescription: {s}\n", .{ (i + 1), books[i].title, books[i].description });
-            try stdout.flush();
-            i += 1;
-        }
-
-        i = 0;
-        while (i < cur_index) {
-            allocator.free(books[i].title);
-            allocator.free(books[i].description);
-            i += 1;
-        }
-
-        allocator.free(books);
+        // {
+        //     var i: u16 = 0;
+        //     while (i < cur_index) {
+        //         allocator.free(books[i].title);
+        //         allocator.free(books[i].description);
+        //         i += 1;
+        //     }
+        //
+        //     allocator.free(books);
+        // }
     }
 
     try stdout.writeAll("Thank you for using Wanshitong!.\n");
